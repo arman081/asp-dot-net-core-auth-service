638537a217ad6288f59bb934dad4e646929fc58c
Arman <arman.cse.081@gmail.com>
2022-06-19 17:08:42 +0600

    dto with attribute validation

---
+++ src/ExtendingIdentityUser.MVC/Dto/RegisterDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExtendingIdentityUser.MVC.Dto
+{
+    public class RegisterDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare]
+        public string ConfirmPassword { get; set; }
+    }
+}
